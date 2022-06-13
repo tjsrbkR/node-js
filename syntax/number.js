a56df2f8ebcb21a47f500d6b2d88fc363078fce6
tjsrbKR <kkil5205@gmail.com>
2022-06-13 17:47:33 +0900

    노드 기초 리터럴,url

---
+++ syntax/number.js
@@ -0,0 +1,6 @@
+console.log(1+1);
+console.log(4-1);
+console.log(2*2);
+console.log(10/3);
+
+
---
+++ syntax/template.js
@@ -0,0 +1,18 @@
+let name = "310101"; 
+let letter = 'Dear '+ name + '\n\n The World Wide Web (abbreviated WWW or the Web) is an information space where documents and other web resources are identified by Uniform Resource Locators (URLs), interlinked by hypertext links, and can be accessed via the Internet.[1] English scientist Tim Berners-Lee invented the World Wide Web in 1989. He wrote the first web browser computer program in 1990 while employed at CERN in Switzerland.[2][3] The Web browser was released outside of CERN in 1991, first to other research institutions starting in January 1991 and to the general public on the Internet in August 19910'; 
+
+
+// `` 안에 ${ 변수 } 를 사용 '+ +'  < 상위호환. 리터럴이라고 부른다. 
+
+
+
+
+
+let letter2 = `Dear + ${name} + '\n\n The World Wide Web (abbreviated WWW or the Web) is an information space where documents and other web resources are identified by Uniform Resource Locators (URLs), interlinked by hypertext links, and can be accessed via the Internet.[1] English scientist Tim Berners-Lee invented the World Wide Web in 1989. He wrote the first web browser computer program in 1990 while employed at CERN in Switzerland.[2][3] The Web browser was released outside of CERN in 1991, first to other research institutions starting in January 1991 and to the general public on the Internet in August ${name}`; 
+
+
+
+
+
+
+console.log(letter);
