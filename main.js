3b1396cd54aabea9f283b54b27aa81cda3cdce14
tjsrbKR <kkil5205@gmail.com>
2022-06-13 17:50:33 +0900

    노드 url,리터럴

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 var http = require('http');
 var fs = require('fs');
+//${변수} < 리터럴 방식
 var url = require('url');
 var app = http.createServer(function(request,response){
     var _url = request.url;
