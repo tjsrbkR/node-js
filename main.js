96c57305307d93dcaae634971dee5e120e48b933
tjsrbKR <kkil5205@gmail.com>
2022-06-15 17:19:29 +0900

    제어문, 조건문 사용해서 쿼리스트링값에 따라 본문 다르게 보여주기

--- main.js
+++ main.js
@@ -5,40 +5,66 @@ var url = require('url');
 var app = http.createServer(function(request,response){
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
+    let pathname = url.parse(_url, true).pathname;
     let title = queryData.id;
-    if(_url == '/'){
-        _url = '/index.html';
-        title = 'Welcome';
-    }
-    if(_url == '/favicon.ico'){
+    if(pathname === '/'){
+        if(title === undefined){
+                let name = "Welcome !";
+                let description = "Hello! Node.js!"
+                let template = `
+                    <!doctype html>
+                    <html>
+                        <head>
+                        <title>WEB1 - ${name}</title>
+                        <meta charset="utf-8">
+                        </head>
+                        <body>
+                        <h1><a href="/">WEB</a></h1>
+                        <ul>
+                            <li><a href="/?id=HTML">HTML</a></li>
+                            <li><a href="/?id=CSS">CSS</a></li>
+                            <li><a href="/?id=JavaScript">JavaScript</a></li>
+                        </ul>
+                        <h2>${name}</h2>
+                        <p>
+                            ${description}
+                        </p>
+                        </body>
+                    </html>
+                `;
+                response.writeHead(200);
+                response.end(template);
+        }
+        else{
+            fs.readFile(`data/${title}`,'utf-8', function(err, description){
+                let template = `
+                    <!doctype html>
+                    <html>
+                        <head>
+                        <title>WEB1 - ${title}</title>
+                        <meta charset="utf-8">
+                        </head>
+                        <body>
+                        <h1><a href="/">WEB</a></h1>
+                        <ul>
+                            <li><a href="/?id=HTML">HTML</a></li>
+                            <li><a href="/?id=CSS">CSS</a></li>
+                            <li><a href="/?id=JavaScript">JavaScript</a></li>
+                        </ul>
+                        <h2>${title}</h2>
+                        <p>
+                            ${description}
+                        </p>
+                        </body>
+                    </html>
+                `;
+                response.writeHead(200);
+                response.end(template);
+            });
+        }
+    } else{
         response.writeHead(404);
-        response.end();
-        return;
+        response.end('Not found');
     }
-    response.writeHead(200);
-    fs.readFile(`data/${title}`,'utf-8', function(err, description){
-        let template = `
-        <!doctype html>
-        <html>
-            <head>
-            <title>WEB1 - ${title}</title>
-            <meta charset="utf-8">
-            </head>
-            <body>
-            <h1><a href="/">WEB</a></h1>
-            <ul>
-                <li><a href="/?id=HTML">HTML</a></li>
-                <li><a href="/?id=CSS">CSS</a></li>
-                <li><a href="/?id=JavaScript">JavaScript</a></li>
-            </ul>
-            <h2>${title}</h2>
-            <p>
-                ${description}
-            </p>
-            </body>
-        </html>
-    `;
-    response.end(template);
-    });
 });
 app.listen(3000);
---
+++ syntax/conditional.js
@@ -0,0 +1,14 @@
+var args = process.argv[2];
+console.log(args);
+
+console.log(1+1);
+if(args[2] === '1'){
+    console.log(4-2);
+}
+else { 
+    console.log(4-1);
+}
+
+console.log(2*2);
+console.log(10/3);
+
